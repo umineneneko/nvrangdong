5a265fd1eddeac8fa4284761dd7503b7f1b9a346
umineneneko@gmail.com <Ngoc Huynh>
2015-03-15 18:14:21 +0700

    comiple=true

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,1 +1,3 @@
 Rails.application.config.assets.precompile += %w( bootstrap.min.js )
+Rails.application.config.assets.precompile += %w( bootstrap.min.css )
+Rails.application.config.assets.precompile += %w( shop-homepage.css )
